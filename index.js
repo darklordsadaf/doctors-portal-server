45f59320ac638dc0bbd507f7af12072b0d4abe4e
Sadaf Rahman <darklordsadaf@gmail.com>
2020-12-25 12:23:19 +0600

    pic done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const fileUpload = require('express-fileupload');
 const MongoClient = require('mongodb').MongoClient;
 require('dotenv').config()
 
@@ -10,6 +11,8 @@ const app = express()
 
 app.use(bodyParser.json());
 app.use(cors());
+app.use(express.static('doctors'));
+app.use(fileUpload());
 
 const port = 5000;
 
@@ -20,6 +23,7 @@ app.get('/', (req, res) => {
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
     const appointmentCollection = client.db("doctorsPortal").collection("appointments");
+    const doctorCollection = client.db("doctorsPortal").collection("doctors");
 
     app.post('/addAppointment', (req, res) => {
         const appointment = req.body;
@@ -38,12 +42,65 @@ client.connect(err => {
 
     app.post('/appointmentByDate', (req, res) => {
         const date = req.body;
-        console.log(date.date);
-        appointmentCollection.find({ date: date.date })
+        const email = req.body.email;
+        // console.log(date.date);
+        doctorCollection.find({ email: email })
+            .toArray((err, doctors) => {
+                const filter = { date: date.date };
+                if (doctors.length === 0) {
+                    filter.email = email;
+                }
+                appointmentCollection.find(filter)
+                    .toArray((err, documents) => {
+                        res.send(documents);
+                    })
+            })
+
+    })
+
+    app.post('/addADoctor', (req, res) => {
+        const file = req.files.file;
+        const name = req.body.name;
+        const email = req.body.email;
+        console.log(name, email, file);
+        // file.mv(`${__dirname}/doctors/${file.name}`, err => {
+        //     if (err) {
+        //         console.log(err);
+        //         return res.status(500).send({ msg: 'Failed to upload Image' });
+        //     }
+        //     return res.send({ name: file.name, path: `/${file.name}` })
+        // })
+        const newImg = file.data;
+        const encImg = newImg.toString('base64');
+
+        var image = {
+            contentType: file.mimetype,
+            size: file.size,
+            img: Buffer.from(encImg, 'base64')
+        };
+
+        doctorCollection.insertOne({ name, email, image })
+            .then(result => {
+                res.send(result.insertedCount > 0);
+            })
+    })
+
+    app.get('/doctors', (req, res) => {
+        doctorCollection.find({})
             .toArray((err, documents) => {
                 res.send(documents);
             })
+    });
+
+    app.post('/isDoctor', (req, res) => {
+        const email = req.body.email;
+        doctorCollection.find({ email: email })
+            .toArray((err, doctors) => {
+                res.send(doctors.length > 0);
+            })
     })
+
+
 
 });
 
